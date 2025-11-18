a084dd321f71f3ace2daa505d77db0dab5efb90e
ianjaggi <iaja6607@colorado.edu>
2025-11-18 13:37:31 -0700

    Add files via upload

--- lab5_main.m
+++ lab5_main.m
@@ -3,44 +3,45 @@
 
 clear; clc; close all;
 
-% 
+
 % Example simulation data setup
-% 
+
 t = linspace(0, 20, 1000)';  % Time vector [s]
 
-% Example aircraft states (12 states)
-% [xE, yE, zE, phi, theta, psi, uE, vE, wE, p, q, r]
-xE = 50 * sin(0.1*t);
-yE = 50 * cos(0.1*t);
-zE = -10 * sin(0.05*t);
-phi = 0.05 * sin(0.5*t);
-theta = 0.03 * sin(0.4*t);
-psi = 0.1 * t;
-uE = 15 + 0.5 * sin(0.3*t);
-vE = 0.2 * sin(0.6*t);
-wE = 0.3 * cos(0.3*t);
-p = 0.1 * sin(0.8*t);
-q = 0.05 * cos(0.9*t);
-r = 0.08 * sin(0.7*t);
+% The aircraft initial state and control inputs are at the following trim values
 
-x = [xE yE zE phi theta psi uE vE wE p q r];
+xE1 = 0; yE1 = 0; zE1 = -1800; % meters
+phi1 = 0; theta1 = deg2rad(1.59282267); psi1 = 0; % degrees
+uE1 = 20.99; vE1 = 0; wE1 = 0.5837; % m/s
+p1 = 0; q1 = 0; r1 = 0; % deg/s
 
-% 
-% Example control input setup
-% 
-% Control inputs: [elevator, aileron, rudder, throttle]
-elevator = deg2rad(2 * sin(0.5*t));  % small deflection in radians
-aileron  = deg2rad(3 * cos(0.4*t));
-rudder   = deg2rad(1 * sin(0.7*t));
-throttle = 0.6 + 0.2*sin(0.3*t);     % varies between 0.4–0.8
+x1 = [xE1 yE1 zE1 phi1 theta1 psi1 uE1 vE1 wE1 p1 q1 r1];
 
-cInp = [elevator aileron rudder throttle];
+elevator1 = 0.1079;
+aileron1  = 0;
+rudder1   = 0;
+throttle1 = 0.3182;
 
-% 
-% Call plotting function
-% 
-fig = 1;           % Figure handle 
-col = 'b';         % Line color/style
+controls1 = [elevator1 aileron1 rudder1 throttle1];
 
-PlotAircraftSim_lab5(t, x, cInp, fig, col);
+% For initial aircraft state and control surface inputs
+
+xE2 = 0; yE2 = 0; zE2 = -1800; % meters
+phi2 = deg2rad(15); theta2 = deg2rad(-12); psi2 = deg2rad(270); % degrees
+uE2 = 19; vE2 = 3; wE2 = -2; % m/s
+p2 = deg2rad(0.08); q2 = deg2rad(-0.2); r2 = 0; % deg/s
+
+x2 = [xE2 yE2 zE2 phi2 theta2 psi2 uE2 vE2 wE2 p2 q2 r2];
+
+elevator2 = deg2rad(5);
+aileron2  = deg2rad(2);
+rudder2   = deg2rad(-13);
+throttle2 = 0.3;
+
+controls2 = [elevator2 aileron2 rudder2 throttle2];
+
+fig = 1; 
+col = 'b'; 
+
+PlotAircraftSim_lab5(t, x1, controls1, fig, col);
 
