b32e7a4496b8756dcaacdd00fbf55d04ce4b211e
ianjaggi <iaja6607@colorado.edu>
2025-11-18 13:05:59 -0700

    Add files via upload

---
+++ FromIntertialToBody.m
@@ -0,0 +1,9 @@
+function [Q] = FromIntertialToBody(phi, theta, psi)
+
+Rphi = [1 0 0; 0 cos(phi) sin(phi); 0 -sin(phi) cos(phi)];
+Rtheta = [cos(theta) 0 -sin(theta); 0 1 0; sin(theta) 0 cos(theta)];
+Rpsi = [cos(psi) sin(psi) 0; -sin(psi) cos(psi) 0; 0 0 1];
+
+Q = Rpsi * Rtheta * Rphi;
+
+end
